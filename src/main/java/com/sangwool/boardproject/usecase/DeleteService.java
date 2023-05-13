48c4427ca5063d22f7686b305898762fb25636c4
OpenTheDoor <tkddnwkdb@gmail.com>
2023-05-13 23:48:04 +0900

    comment: 삭제 관련된 기능을 처리하는 usecase 레벨 주석 추가

--- src/main/java/com/sangwool/boardproject/usecase/DeleteService.java
+++ src/main/java/com/sangwool/boardproject/usecase/DeleteService.java
@@ -2,9 +2,27 @@ package com.sangwool.boardproject.usecase;
 
 public interface DeleteService {
 
+    /**
+     * boardDelete - DeleteUseCase |
+     * 게시글을 삭제한다.
+     *
+     * @param boardSeq 게시글 Sequence 번호를 받아온다.
+     */
     Boolean boardDelete(Long boardSeq);
 
+    /**
+     * commentDelete - DeleteUseCase |
+     * 댓글을 삭제한다.
+     *
+     * @param commentSeq 댓글 Sequence 번호를 받아온다.
+     */
     Boolean commentDelete(Long commentSeq);
 
+    /**
+     * nestedCommentDelete - DeleteUseCase |
+     * 대댓글을 삭제한다.
+     *
+     * @param nestedCommentSeq 대댓글 Sequence 번호를 받아온다.
+     */
     void nestedCommentDelete(Long nestedCommentSeq);
 }
